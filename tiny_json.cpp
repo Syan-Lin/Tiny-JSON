f45a7b2766b74f972e1281b5702694136657cf11
SyanLin <475694569@qq.com>
2022-08-24 01:35:14 +0800

    ✨ Implement classes.
    Boolean、Null、String、Number

--- tiny_json.cpp
+++ tiny_json.cpp
@@ -1,8 +1,308 @@
+
+/**************************
+* @author   Yuan.
+* @date     2022/8/24
+* @brief    tiny_json 实现
+* @version  v0.1 alpha
+***************************/
+
 #include "tiny_json.h"
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+
+bool tiny_json::DEBUG = false;
+bool tiny_json::JSON5 = true;
 
 // 判断对象是否可转化为字符串（可能出现环导致死循环）
-static bool parseable(const tiny_json::Object&);
+static bool parseable(const tiny_json::Object&){
+    return true;
+}
 // 判断字符串是否可转化为对象
 static bool parseable(std::string){
     return true;
 }
+std::string tiny_json::parse(const tiny_json::Object& obj){
+    return "hhh";
+}
+// tiny_json::Object& tiny_json::parse(const std::string str){
+//     return Object();
+// }
+
+/**************************
+* @author   Yuan.
+* @date     2022/8/24
+* @brief    Number 类实现
+***************************/
+
+// 拷贝控制成员
+tiny_json::Number::Number(const double val){ num_ = val; }
+tiny_json::Number::Number(const Number& val){ num_ = val.get(); }
+tiny_json::Number& tiny_json::Number::operator=(const Number& val){
+    num_ = val.get();
+    return *this;
+}
+tiny_json::Number& tiny_json::Number::operator=(const double val){
+    num_ = val;
+    return *this;
+}
+
+// 功能成员
+void tiny_json::Number::set(const double val){ num_ = val; }
+double tiny_json::Number::get() const { return num_; }
+void tiny_json::Number::reset(){ num_ = 0; }
+
+std::string tiny_json::Number::parse(){
+    std::string str;
+    std::stringstream ss;
+
+    switch(type_){
+        case NumberType::kDefault:{
+            ss << num_;
+            str = ss.str();
+            break;
+        }
+        case NumberType::kInteger:{
+            long long integer = static_cast<long long>(num_);
+            ss << integer;
+            str = ss.str();
+            break;
+        }
+        case NumberType::kFloat:{
+            ss << std::setprecision(decimal_place_) << num_;
+            str = ss.str();
+            break;
+        }
+    }
+    return str;
+}
+bool tiny_json::Number::parseable(std::string val) const {
+    return true;
+}
+bool tiny_json::Number::parseable() const { return true; }
+void tiny_json::Number::parseSetting(NumberType type, size_t decimal_place){
+    type_ = type;
+    decimal_place_ = decimal_place;
+}
+
+/**************************
+* @author   Yuan.
+* @date     2022/8/23
+* @brief    Null 类实现
+***************************/
+
+std::string tiny_json::Null::parse(){ return "null"; }
+bool tiny_json::Null::parseable(std::string val) const { return val == "null" ? true : false; }
+bool tiny_json::Null::parseable() const { return true; }
+
+/**************************
+* @author   Yuan.
+* @date     2022/8/24
+* @brief    String 类实现
+***************************/
+
+// 拷贝控制成员
+tiny_json::String::String(const std::string& val){
+    str_ = val;
+    parse();
+}
+tiny_json::String::String(const String& val){ str_ = val.get(); }
+tiny_json::String::String(String&& val) noexcept{ str_ = std::move(val.get()); }
+tiny_json::String::String(std::string&& val) noexcept{
+    str_ = std::move(val);
+    parse();
+}
+tiny_json::String& tiny_json::String::operator=(const String& val){
+    str_ = val.get();
+    return *this;
+}
+tiny_json::String& tiny_json::String::operator=(String&& val) noexcept{
+    str_ = std::move(val.get());
+    return *this;
+}
+tiny_json::String& tiny_json::String::operator=(const std::string& val){
+    str_ = val;
+    parse();
+    return *this;
+}
+tiny_json::String& tiny_json::String::operator=(std::string&& val) noexcept{
+    str_ = std::move(val);
+    parse();
+    return *this;
+}
+
+// 功能成员
+void tiny_json::String::set(const std::string& val){
+    str_ = val;
+    is_parsed_ = true;
+}
+std::string tiny_json::String::get() const { return str_; }
+void tiny_json::String::reset(){ str_.clear(); }
+
+std::string tiny_json::String::parse() {
+    if(is_parsed_){
+        return parsed_str_;
+    }else if(!parseable()){
+        return "";
+    }
+    is_parsed_ = true;
+    parseForPrint();
+    parseForFile();
+
+    return parsed_str_;
+}
+void tiny_json::String::parseForPrint(){
+    std::string str;
+    for(int i = 0; i < str_.size(); i++){
+        if(str_[i] == '\\'){
+            i++;
+            switch(str_[i]){
+                case 'b':
+                    str.push_back('\b');
+                    break;
+                case 'f':
+                    str.push_back('\f');
+                    break;
+                case 't':
+                    str.push_back('\t');
+                    break;
+                case 'n':
+                    str.push_back('\n');
+                    break;
+                case 'r':
+                    str.push_back('\r');
+                    break;
+                case '"':
+                    str.push_back('"');
+                    break;
+                case '\\':
+                    str.push_back('\\');
+                    break;
+                case 'u':{
+                    std::stringstream ss;
+                    for(int k = 0; k < 4; k++){
+                        i++;
+                        ss << std::hex << str_[i];
+                    }
+                    char ch;
+                    ss >> ch;
+                    str.push_back(ch);
+                    break;
+                }
+            }
+        }else{
+            str.push_back(str_[i]);
+        }
+    }
+    str_ = str;
+}
+void tiny_json::String::parseForFile(){
+    std::string str;
+    for(int i = 0; i < str_.size(); i++){
+        switch(str_[i]){
+            case '\b':
+                str.push_back('\\');
+                str.push_back('b');
+                break;
+            case '\f':
+                str.push_back('\\');
+                str.push_back('f');
+                break;
+            case '\t':
+                str.push_back('\\');
+                str.push_back('t');
+                break;
+            case '\n':
+                str.push_back('\\');
+                str.push_back('n');
+                break;
+            case '\r':
+                str.push_back('\\');
+                str.push_back('r');
+                break;
+            case '"':
+                str.push_back('\\');
+                str.push_back('"');
+                break;
+            case '\\':
+                str.push_back('\\');
+                str.push_back('\\');
+                break;
+            default:
+                str.push_back(str_[i]);
+                break;
+        }
+    }
+    parsed_str_ = str;
+}
+bool tiny_json::String::parseable(std::string str) const {
+    for(int i = 0; i < str_.size(); i++){
+        if(str_[i] == '\\'){
+            i++;
+            switch(str_[i]){
+                case 'b': case 'f': case 't':
+                case 'n': case 'r': case '"':
+                case '\\': break;
+                case 'u':{
+                    std::stringstream ss;
+                    int k = 0;
+                    for(; k < 4 && i < str_.size(); k++){
+                        i++;
+                        ss << std::hex << str_[i];
+                    }
+                    // 不满足 \uxxxx 格式
+                    if(k != 4){
+                        std::cout << "[tiny_json_Error]: 字符串 " << str_
+                        << " 不能转化为 String 对象!(错误类型: Unicode 非法)" << std::endl;
+                        return false;
+                    }
+                    break;
+                }
+                default:
+                    std::cout << "[tiny_json_Error]: 字符串 " << str_
+                    << " 不能转化为 String 对象!(错误类型: 转义字符非法)" << std::endl;
+                    return false;
+                    break;
+            }
+        }else if(str_[i] == '"'){
+            std::cout << "[tiny_json_Error]: 字符串 " << str_
+            << " 不能转化为 String 对象!(错误类型: 转义字符非法)" << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
+bool tiny_json::String::parseable() const { return parseable(str_); }
+
+
+/**************************
+* @author   Yuan.
+* @date     2022/8/23
+* @brief    Boolean 类实现
+***************************/
+
+// 拷贝控制成员
+tiny_json::Boolean::Boolean(const bool val){ bool_ = val; }
+tiny_json::Boolean::Boolean(const std::string& val){
+    if(!parseable(val)){
+        std::cout << "[tiny_json_Error]: 字符串 " << val << " 不能转化为 Boolean 对象!" << std::endl;
+        return;
+    }
+    bool_ = val == "true" ? "true" : "false";
+}
+tiny_json::Boolean::Boolean(const Boolean& val){ bool_ = val.get(); }
+tiny_json::Boolean& tiny_json::Boolean::operator=(const Boolean& val){
+    bool_ = val.get();
+    return *this;
+}
+
+// 功能成员
+void tiny_json::Boolean::set(const bool val){ bool_ = val; }
+bool tiny_json::Boolean::get() const { return bool_; }
+void tiny_json::Boolean::reset(){ bool_ = false; }
+
+std::string tiny_json::Boolean::parse(){ return bool_ ? "true" : "false"; }
+bool tiny_json::Boolean::parseable(std::string val) const {
+    return (val == "true" || val == "false") ? true : false;
+}
+bool tiny_json::Boolean::parseable() const { return true; }
--- tiny_json.h
+++ tiny_json.h
@@ -8,9 +8,9 @@
 namespace tiny_json{
 
 // 是否输出调试信息
-bool DEBUG = false;
+extern bool DEBUG;
 // 是否使用 JSON5 标准
-bool JSON5 = false;
+extern bool JSON5;
 
 // JSON 数据类型
 class Value;
@@ -22,7 +22,7 @@ class String;
 class Boolean;
 
 // JSON 数据类型枚举
-enum class Type {
+enum class Type{
     kNumber,
     kBoolean,
     kString,
@@ -31,16 +31,26 @@ enum class Type {
     kArray
 };
 
+enum class NumberType{
+    kInteger,   // 整数类型
+    kFloat,     // 浮点数类型，可指定位数
+    kDefault    // 默认类型，六位浮点数
+};
+
 // 功能接口
-class Parseable {
+class Parseable{
 public:
     // 将对象输出为字符串
     virtual std::string parse() = 0;
+    // JSON 文件是否能转化为对象
+    virtual bool parseable(std::string) const = 0;
+    // 对象是否能转化为 JSON 文件
+    virtual bool parseable() const = 0;
 };
 
 // JSON 键值对中的值：
 // 可以是 Number、Boolean、String、Null、Object、Array
-class Value : public Parseable {
+class Value : public Parseable{
     template <typename T> using ptr = std::unique_ptr<T>;
 public:
     // 拷贝控制成员
@@ -66,9 +76,9 @@ public:
     void set(const Array&);
     void set(const Value&);
     // 获取值类型
-    Type getType();
+    Type getType() const;
     // 获取值
-    template <typename T> T& get();
+    template <typename T> T& get() const;
     // 重置值为 Null
     void reset();
 
@@ -77,7 +87,7 @@ public:
 
 private:
     Type type_;
-    union {
+    union{
         ptr<Number> number_val_;
         ptr<Boolean> boolean_val_;
         ptr<String> string_val_;
@@ -88,7 +98,7 @@ private:
 };
 
 // JSON 的键值对集合
-class Object : public Parseable {
+class Object : public Parseable{
     typedef std::string Key;
     typedef std::map<std::string, std::unique_ptr<Value>> kvMap;
 public:
@@ -107,6 +117,8 @@ public:
     void del(const Key&);
     // 设置键值对，如果不存在则创建一个
     void set(const Key&, const Value&);
+    // 根据键获得值
+    Value& get(const Key&) const;
     // 清空键值对集合
     void reset();
 
@@ -118,7 +130,7 @@ private:
 };
 
 // 存储 Value 的数组
-class Array : public Parseable {
+class Array : public Parseable{
     typedef std::vector<std::unique_ptr<Value>> Vector;
 public:
     // 拷贝控制成员
@@ -135,11 +147,15 @@ public:
     // 删除尾部元素
     void pop();
     // 在指定位置之前添加元素
-    void add(int, const Value&);
+    void add(size_t, const Value&);
     // 删除指定位置的元素
-    void del(int, const Value&);
+    void del(size_t, const Value&);
     // 设置指定位置的元素
-    void set(int, const Value&);
+    void set(size_t, const Value&);
+    // 获取数组大小
+    size_t size() const;
+    // 获取元素
+    Value& get(size_t) const;
     // 清空数组
     void reset();
 
@@ -151,84 +167,112 @@ private:
 };
 
 // 数字类型，整数或浮点数
-class Number : public Parseable {
+class Number : public Parseable{
 public:
     // 拷贝控制成员
-    Number();
-    Number(const long double);
+    Number() = default;
+    Number(const double);
     Number(const Number&);                    // 拷贝构造
-    Number(Number&&) noexcept;                // 移动构造
-    Number& operator=(Number&);               // 拷贝赋值
-    Number& operator=(Number&&) noexcept;     // 移动赋值
-    ~Number();
+    Number& operator=(const Number&);         // 拷贝赋值
+    Number& operator=(const double);
+    ~Number() = default;
 
     // 设置数字
-    void set(const long double);
+    void set(const double);
     // 获取数字
-    long double get();
+    double get() const;
     // 重置数字
     void reset();
 
     // 将数字输出为字符串
     std::string parse() override;
+    // JSON 对象能够转化为 Number 对象
+    bool parseable(std::string) const override;
+    // Number 对象能否转化为 JSON 对象
+    bool parseable() const override;
+    // 设置数字输出格式
+    void parseSetting(NumberType, size_t = 6);
 
 private:
-    long double num_;
+    double num_;
+    NumberType type_ = NumberType::kDefault;
+    size_t decimal_place_ = 6;
 };
 
 // 空类型
-class Null : public Parseable {
+class Null : public Parseable{
 public:
     // 输出 Null 字符串
     std::string parse() override;
+    // JSON 对象能够转化为 Null 对象
+    bool parseable(std::string) const override;
+    // Null 对象能否转化为 JSON 对象
+    bool parseable() const override;
 };
 
 // 字符串类型
-class String : public Parseable {
+// 特殊字符需要转义: \\n、\\t、\\r、\\u、\\、\"
+class String : public Parseable{
 public:
     // 拷贝控制成员
-    String();
+    String() = default;
     String(const std::string&);
-    String(const String&);                    // 拷贝构造
-    String(String&&) noexcept;                // 移动构造
-    String& operator=(String&);               // 拷贝赋值
-    String& operator=(String&&) noexcept;     // 移动赋值
-    ~String();
+    String(const String&);                      // 拷贝构造
+    String(String&&) noexcept;                  // 移动构造
+    String(std::string&&) noexcept;             // 移动构造
+    String& operator=(const String&);           // 拷贝赋值
+    String& operator=(String&&) noexcept;       // 移动赋值
+    String& operator=(const std::string&);      // 拷贝赋值
+    String& operator=(std::string&&) noexcept;  // 移动赋值
+    ~String() = default;
 
     // 设置字符串
     void set(const std::string&);
     // 获取字符串
-    std::string get();
+    std::string get() const;
     // 重置字符串
     void reset();
 
     // 输出字符串
     std::string parse() override;
+    // JSON 对象能够转化为 String 对象
+    bool parseable(std::string) const override;
+    // String 对象能否转化为 JSON 对象
+    bool parseable() const override;
+
 private:
     std::string str_;
+    std::string parsed_str_;
+    bool is_parsed_ = false;
+    void parseForPrint();
+    void parseForFile();
 };
 
 // 布尔类型
-class Boolean : public Parseable {
+class Boolean : public Parseable{
 public:
     // 拷贝控制成员
-    Boolean();
+    Boolean() = default;
     Boolean(const bool);
+    Boolean(const std::string&);
     Boolean(const Boolean&);                    // 拷贝构造
-    Boolean(Boolean&&) noexcept;                // 移动构造
-    Boolean& operator=(Boolean&);               // 拷贝赋值
-    Boolean& operator=(Boolean&&) noexcept;     // 移动赋值
-    ~Boolean();
+    Boolean& operator=(const Boolean&);         // 拷贝赋值
+    ~Boolean() = default;
 
     // 设置布尔值
     void set(const bool);
     // 获取布尔值
-    bool get();
+    bool get() const;
     // 重置布尔值
     void reset();
 
     // 将布尔值输出为字符串
     std::string parse() override;
+    // JSON 对象能够转化为 Boolean 对象
+    bool parseable(std::string) const override;
+    // Boolean 对象能否转化为 JSON 对象
+    bool parseable() const override;
+
 private:
     bool bool_;
 };
@@ -236,6 +280,6 @@ private:
 // 将对象转化为字符串
 extern std::string parse(const Object&);
 // 将字符串转化为对象
-extern Object& parse(std::string);
+extern Object& parse(const std::string);
 
 }
