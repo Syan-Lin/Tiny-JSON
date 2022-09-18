2d6b83c052b4b3d4bee38518264f4b360810a90b
SyanLin <475694569@qq.com>
2022-09-18 17:05:05 +0800

    ✨ Add function tests frame.

--- tiny_json_frame.h
+++ tiny_json_frame.h
@@ -23,7 +23,7 @@ public:
     // 打印确认信息
     static void check(std::string, std::string);
     // 打印信息
-    static void print(std::string, Color);
+    static void print(std::string, Color = Color::kDefault);
 private:
     static HANDLE outHandle;
     static void setColor(Color);
@@ -35,5 +35,28 @@ namespace tiny_json_test{
 
 using namespace tiny_json_log;
 
+#define __DEBUG_INFO__ std::string()
+
+class Test{
+public:
+    Test(std::string);
+    void printResult();
+    // 断言
+    void ExpectTrue(bool, std::string = "");
+    void ExpectFalse(bool, std::string = "");
+    void ExpectInt(int, int, std::string = "");
+    void ExpectDouble(double, double, std::string = "");
+    void ExpectString(std::string, std::string, std::string = "");
+
+public:
+    const std::string name_;
+    int failed_count_ = 0;
+    int passed_count_ = 0;
+    static bool show_details_;
+};
+
+class PerformanceTest : public Test{
+
+};
 
 }
